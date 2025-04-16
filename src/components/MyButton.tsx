1d2c779d1350baf49f087f7b0afee167a8dda64a
yahomi-dev <yahomi.dev@gmail.com>
2025-04-16 23:12:53 +0900

    first component

---
+++ src/components/MyButton.tsx
@@ -0,0 +1,12 @@
+function MyButton() {
+	return (
+		<button
+			type="button"
+			className="px-2 border bg-gray-200 rounded-sm hover:bg-gray-400"
+		>
+			I'm a button
+		</button>
+	);
+}
+
+export { MyButton };
